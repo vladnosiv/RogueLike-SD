ad680511557e09d5a59f8896b2eaa3fd21cae243
Cabpacob <Cabpacob@github.com>
2022-04-09 02:01:53 +0300

    Add map generators

---
+++ roguelike/src/main/kotlin/model/FloorMapGenerator.kt
@@ -0,0 +1,8 @@
+package model
+
+class FloorMapGenerator: MapGenerator {
+    override fun genMap(config: MapGeneratorConfig): Map {
+        val field = List(config.height) { List(config.weight) { Tile(TileType.FLOOR) } }
+        return Map(field)
+    }
+}
---
+++ roguelike/src/main/kotlin/model/MapGeneratorConfig.kt
@@ -0,0 +1,3 @@
+package model
+
+data class MapGeneratorConfig(val height: Int, val weight: Int)
