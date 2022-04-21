5d28728eacf93681fdd7ec110d606c9d94bbd67c
Cabpacob <Cabpacob@gmail.com>
2022-04-21 21:46:12 +0300

    return necessary actions

--- src/commonMain/kotlin/model/ModelHandler.kt
+++ src/commonMain/kotlin/model/ModelHandler.kt
@@ -2,18 +2,27 @@ package model
 
 import model.actions.Action
 import model.actions.HeroMoved
+import model.actions.HeroPlaced
 import model.actions.MapChanged
 
 // class that stores the model
 class ModelHandler {
     private val logic = ModelLogic()
+    private var actions = mutableListOf<Action>()
+    private var canMove = true
 
-    private var actions = mutableListOf<Action>(MapChanged(FloorMapGenerator(MapGeneratorConfig(32, 32)).genMap().field))
+    init {
+        actions = mutableListOf(
+            MapChanged(FloorMapGenerator(MapGeneratorConfig(32, 32)).genMap().field),
+            HeroPlaced(Position(16, 16))
+        )
+    }
 
     // handles move action
     fun onMove(move: Move) {
-        if (logic.canMainCharacterMove(move)) {
+        if (logic.canMainCharacterMove(move) && canMove) {
             actions.addAll(logic.mainCharacterMove(move))
+            canMove = false
         }
     }
 
@@ -22,8 +31,23 @@ class ModelHandler {
     // returns all actions from last tick
     fun onTick(): List<Action> {
         val actionsFromLastTick = actions
+
         actions = mutableListOf()
-        logic.tick()
-        return actionsFromLastTick.reversed().distinctBy { action -> action.javaClass } // TODO("Ensure one ActorMoved action on list")
+        canMove = true
+
+        actionsFromLastTick.addAll(logic.tick())
+        // TODO("Ensure one HeroMoved action on list")
+        for (action in actions) {
+            actions.addAll(handleAction(action))
+        }
+        return actionsFromLastTick
     }
+
+    private fun handleAction(action: Action): List<Action> {
+        return when (action) { // TODO("Other actions")
+            is HeroMoved -> emptyList()
+            else -> throw Exception()
+        }
+    }
+
 }
