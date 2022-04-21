b06a6be7f6563a06d20ca84eba3440a48ce6b26d
Cabpacob <Cabpacob@gmail.com>
2022-04-21 22:33:03 +0300

    Fix bug

--- src/commonMain/kotlin/model/map/FloorMapGenerator.kt
+++ src/commonMain/kotlin/model/map/FloorMapGenerator.kt
@@ -5,9 +5,13 @@ class FloorMapGenerator(private val config: MapGeneratorConfig) : MapGenerator {
     override fun genMap(): Map {
         val height = config.height
         val width = config.width
-        return Map(List(width) { x -> List(height) { y -> when {
-            x == 0 || x == width - 1 || y == 0 || y == height - 1 -> Tile(TileType.WALL, x, y)
-            else -> Tile(TileType.FLOOR, x, y)
-        }}})
+        return Map(List(width) { x ->
+            List(height) { y ->
+                when {
+                    x == 0 || x == width - 1 || y == 0 || y == height - 1 -> Tile(TileType.WALL, x, y)
+                    else -> Tile(TileType.FLOOR, x, y)
+                }
+            }
+        })
     }
 }
--- src/commonMain/kotlin/model/map/Map.kt
+++ src/commonMain/kotlin/model/map/Map.kt
@@ -25,7 +25,7 @@ data class Map(val field: List<List<Tile>>) {
 
     fun isPositionOnField(position: Position): Boolean {
         val yOK = (0 <= position.y && position.y < field.size)
-        val xOK = (0 <= position.x && position.x < field[position.y].size)
+        val xOK = yOK && (0 <= position.x && position.x < field[position.y].size)
         return xOK && yOK
     }
 
