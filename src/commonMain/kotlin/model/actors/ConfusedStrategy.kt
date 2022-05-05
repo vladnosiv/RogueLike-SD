516ff9c4f5cd3d5f9925b9126cb4c913c758f739
Cabpacob <Cabpacob@gmail.com>
2022-05-06 00:45:23 +0300

    add confused strategy

---
+++ src/commonMain/kotlin/model/actors/ConfusedStrategy.kt
@@ -0,0 +1,25 @@
+package model.actors
+
+import model.Direction
+import model.Environment
+import model.actions.Action
+import model.actions.MobAttacked
+import model.actions.MobMoved
+
+class ConfusedStrategy(override val environment: Environment) : Strategy {
+    override fun makeAct(mob: Mob): List<Action> {
+        val mobPos = mob.position
+
+        for (direction in Direction.values().shuffled()) {
+            val pos = mobPos + direction
+            if (environment.map.canStep(pos)) {
+//                mob.position += direction
+                return listOf(
+                    MobMoved(mob, direction.deltaX, direction.deltaY, direction)
+                )
+            }
+        }
+
+        return emptyList()
+    }
+}
